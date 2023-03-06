24721c67dde67ea45d48566b3f7788bc11a083e6
ChungNguyenVr912 <chungdeser@gmail.com>
2023-03-06 14:32:34 +0700

    add bt hien thi dau ra

---
+++ demo-output.js
@@ -0,0 +1,16 @@
+function showalert()
+{
+    alert('xin chao');
+}
+function changetext()
+{
+    document.getElementById('changetxt').innerHTML='Xin chao';
+}
+function docwrite()
+{
+    document.write('Rat xin chao');
+}
+function showlog()
+{
+    console.log('Minh rat xin chao');
+}
